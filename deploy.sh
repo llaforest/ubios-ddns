8d8055eab1badf827281407f7c2efc8be887d8be
llaforest <l.laforest@gmail.com>
2023-03-11 14:31:32 -0500

    Modify deployment to support all UnifiOS versions

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 set -e
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(dirname ${0})
 
-case "$(ubnt-device-info model || true)" in
-  "UniFi Dream Machine Pro"|"UniFi Dream Machine")
-    echo "UDM/Pro detected, installing ddns in /mnt/data..."
-    DATA_DIR="/mnt/data"
-    ;;
-  "UniFi Dream Router"|"UniFi Dream Machine SE")
-    echo "UDR/UDMSE detected, installing ddns in /data..."
+# Set default data path
+export DATA_DIR="/mnt/data"
+# Get the firmware version
+export FIRMWARE_VER=$(ubnt-device-info firmware || true)
+# Get the Harware Model
+export MODEL="$(ubnt-device-info model || true)"
+
+if [ $(echo ${FIRMWARE_VER} | sed 's#\..*$##g') -gt 1 ]
+then
     sed -i 's#/mnt/data#/data#g' "${SCRIPT_DIR}/ubios-ddns/ubios-ddns.env" "${SCRIPT_DIR}/ubios-ddns/ubios-ddns.sh" "${SCRIPT_DIR}/ubios-ddns/on_boot.d/98-ubios-ddns.sh"
-    sed -i 's#/etc/init.d/crond#/etc/init.d/cron#g' "${SCRIPT_DIR}/ubios-ddns/ubios-ddns.env" "${SCRIPT_DIR}/ubios-ddns/ubios-ddns.sh"
-    DATA_DIR="/data"
+    sed -i 's#/etc/init.d/crond#/etc/init.d/cron#g' "${SCRIPT_DIR}/ubios-ddns/ubios-ddns.sh"
+    export DATA_DIR="/data"
+fi
+
+case "${MODEL}" in
+    "UniFi Dream Machine Pro"|"UniFi Dream Machine"|"UniFi Dream Router"|"UniFi Dream Machine SE")
+    echo "${MODEL} running firmware ${FIRMWARE_VER} detected, installing ubios-cert in ${DATA_DIR}..."
     ;;
-  *)
-    echo "Unsupported model: $(ubnt-device-info model)" 1>&2
+    *)
+    echo "Unsupported model: ${MODEL}"
     exit 1
     ;;
 esac
@@ -24,4 +31,4 @@ mkdir -p "${DATA_DIR}/ubios-ddns"
 mv "${SCRIPT_DIR}/ubios-ddns" "${DATA_DIR}"
 cd "${DATA_DIR}/ubios-ddns"
 chmod 0755 ubios-ddns.sh on_boot.d/98-ubios-ddns.sh
-rm -irf ${SCRIPT_DIR}/../ubios-ddns-main*
+echo "Deployed with success in ${DATA_DIR}/ubios-ddns"
