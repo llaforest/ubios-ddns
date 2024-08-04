9c3dc6648113940ea9cd3f4d79118b4189f67c17
Laurence Laforest <l.laforest@gmail.com>
2024-08-04 07:41:52 -0400

    Hardcode script path

--- on_boot.d/98-ubios-ddns.sh
+++ on_boot.d/98-ubios-ddns.sh
@@ -6,5 +6,5 @@
 if [ ! -f /etc/cron.d/ubios-ddns ]; then
 	# Sleep for 1 minute to allow Internet connection to be up.
 	sleep 60
-	sh ${UBIOS_DDNS_ROOT}/ubios-ddns.sh
+	sh /data/ubios-ddns/ubios-ddns.sh
 fi
